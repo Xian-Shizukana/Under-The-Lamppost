e84964615ddc2be06967c327a452562ca99ac8ad
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-02-11 10:35:36 +0800

    Added more pages

--- Assets/Script/ButtonScript.cs
+++ Assets/Script/ButtonScript.cs
@@ -9,16 +9,24 @@ public class ButtonScript : MonoBehaviour
     public GameObject Page_1;
     public GameObject Page_2;
     public GameObject Page_3;
-
+    public GameObject Page_4;
+    public GameObject Page_5;
+    public GameObject Page_6;
+    public GameObject Page_7;
     public GameObject EndScreen;
 
     // Start is called before the first frame update
     public void Start()
     {
         Page_1.SetActive(true);
+        EndScreen.SetActive(false);
         Page_2.SetActive(false);
         Page_3.SetActive(false);
-        EndScreen.SetActive(false);
+        Page_4.SetActive(false);
+        Page_5.SetActive(false);
+        Page_6.SetActive(false);
+        Page_7.SetActive(false);
+     
     }
 
     public void PageTwo()
@@ -33,11 +41,26 @@ public class ButtonScript : MonoBehaviour
         Page_3.SetActive(true);
     }
 
+    public void PageFour()
+    {
+        Page_3.SetActive(false);
+        Page_4.SetActive(true);
+    }
+    public void PageFive()
+    {
+        Page_4.SetActive(false);
+        Page_5.SetActive(true);
+    }
+
     public void End()
     {
+        EndScreen.SetActive(true);
         Page_1.SetActive(false);
         Page_2.SetActive(false);
         Page_3.SetActive(false);
-        EndScreen.SetActive(true);
+        Page_4.SetActive(false);
+        Page_5.SetActive(false);
+        Page_6.SetActive(false);
+        Page_7.SetActive(false);
     }
 }
